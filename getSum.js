03980f82351d28d47270adeaa1bf04eb10ce28a2
DimaZ <dimazubkov99@gmail.com>
2021-08-19 17:10:08 +0300

    first commit

---
+++ getSum.js
@@ -0,0 +1,19 @@
+function getSum( a,b )
+{
+   let result = 0;
+
+    if(a == b) {
+        return a
+    }
+  
+    if( a < b) {
+      for(let i = a; i <= b; i++) {
+        result += i;
+    }} else if (a > b) {
+      for (let i = b; i <= a; i++) {
+        result += i;
+      }
+    }
+
+    return result;
+}
