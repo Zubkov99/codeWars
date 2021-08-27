49a65296dbe9cb59583c63ed116600953c4b4e0c
DimaZ <dimazubkov99@gmail.com>
2021-08-27 16:32:38 +0300

    added formatStringName

---
+++ formatStringName.js
@@ -0,0 +1,29 @@
+function list(names) {  
+    if (names.length == 1) {
+        return names[0].name
+    } else if (names.length == 0) {
+        return ''
+    } 
+
+    let arr = names.map(item => {
+        return item = item.name
+      })
+    
+    arr.splice(arr.length - 2, 2, (arr[arr.length - 2] + ' & ' + arr[arr.length - 1]));
+
+    return arr.join(', ');
+}
+
+
+
+
+console.log(list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ]));
+
+list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
+
+
+
+// console.log(list([ {name: 'Bart'} ]));
+
+// console.log(list([]));
+
